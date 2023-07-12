140723576260cd9954e5e7d7cb4c33960f2505d2
Bjørn Rudi Dahl <bjorn.rudi.dahl@outlook.com>
2023-07-12 13:12:48 +0200

    Adding test-projects (#2)
    Co-authored-by: Bjørn Rudi Dahl <bjorn.rudi.dahl@observemedical.com>

---
+++ testing/powerprice-cs-server-service-testing/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace powerprice_cs_server_service_testing;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ testing/powerprice-cs-server-testing/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace powerprice_cs_server_testing;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
