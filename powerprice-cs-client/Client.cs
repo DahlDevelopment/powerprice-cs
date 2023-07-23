c407d7ffce792c4cc9ae95a79f520ce1576e23de
bjornrd <bjorn.rudi.dahl@outlook.com>
2023-07-23 14:24:22 +0200

    PPC-3: Adding powerprice.proto to client side

--- powerprice-cs-client/Client.cs
+++ powerprice-cs-client/Client.cs
@@ -1,12 +1,16 @@
-﻿namespace powerprice_cs_client
+﻿using Grpc.Net.Client;
+
+namespace powerprice_cs_client
 {
     public class Client
     {
         private readonly IComMethod _comMethod;
+        private readonly GrpcChannel _grpcChannel;
 
         public Client(IComMethod comMethod)
         {
             _comMethod = comMethod;
+            _grpcChannel = GrpcChannel.ForAddress("http://localhost:5288"); //TODO: Need to figure out how to set the correct address for remote hosts
         }
 
         public IComResponse Query(IComCommand command)
