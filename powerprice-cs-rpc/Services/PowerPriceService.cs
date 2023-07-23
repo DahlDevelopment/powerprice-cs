813ae31e5b5ebd60bdc261e3f0dbf94513803914
bjornrd <bjorn.rudi.dahl@outlook.com>
2023-07-23 11:11:13 +0200

    PPC-3: Adding gRPC server
    Including stub for GetPriceData

---
+++ powerprice-cs-rpc/Services/PowerPriceService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using powerprice_cs_rpc;
+
+namespace powerprice_cs_rpc.Services;
+
+public class PowerPriceService : PriceData.PriceDataBase
+{
+    private readonly ILogger<PowerPriceService> _logger;
+    public PowerPriceService(ILogger<PowerPriceService> logger)
+    {
+        _logger = logger;
+    }
+
+
+    public override Task<PriceDataReply> GetPriceData(PriceDataRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new PriceDataReply
+        {
+            PriceData = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+        });
+    }
+}
+
