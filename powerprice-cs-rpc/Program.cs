a2d9e3982a4788b4c8c8e296e7ad83af121398a2
bjornrd <bjorn.rudi.dahl@outlook.com>
2023-07-23 14:22:22 +0200

    PPC-3: Moving comment

--- powerprice-cs-rpc/Program.cs
+++ powerprice-cs-rpc/Program.cs
@@ -3,6 +3,9 @@ using powerprice_cs_rpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 builder.WebHost.ConfigureKestrel(options =>
 {
     // Setup a HTTP/2 endpoint without TLS.
@@ -10,8 +13,6 @@ builder.WebHost.ConfigureKestrel(options =>
         HttpProtocols.Http2);
 });
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
 builder.Services.AddGrpc();
